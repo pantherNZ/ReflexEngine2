f1284c0dc4f84a633d29d1a69cf0083cc6b29b03
Alex Denford <pantherNZ@users.noreply.github.com>
2021-05-01 13:20:35 +1200

    Start of asynchronous resource loading

--- ReflexEngine/ReflexEngine/ResourceManager.h
+++ ReflexEngine/ReflexEngine/ResourceManager.h
@@ -5,7 +5,7 @@
 
 namespace Reflex
 {
-	enum class ResourceID : unsigned short;
+	typedef unsigned ResourceID;
 
 	namespace Core
 	{
@@ -19,21 +19,37 @@ namespace Reflex
 		class ResouceManager
 		{
 		public:
-			// Resource loading
+			typedef std::map< ResourceID, std::unique_ptr< Resource > > ResourceMap;
+
 			Resource& LoadResource( const ResourceID id, const std::string& filename );
+			void LoadResourceAsync( const ResourceID id, const std::string& filename );
 
 			// Resource loading with a second parameter (for shaders, or textures that specify a bounding rect etc.)
 			template< typename Parameter >
 			Resource& LoadResource( const ResourceID id, const std::string& filename, const Parameter& secondParam );
 
-			// Fetches a resource from the map
+			template< typename Parameter >
+			void LoadResourceAsync( const ResourceID id, const std::string& filename, const Parameter& secondParam );
+
 			const Resource& GetResource( const ResourceID id ) const;
 
+			template< typename Resource >
+			struct ThreadData
+			{
+				ResouceManager< Resource >* manager;
+				ResourceID id;
+				std::string filename;
+			};
+
 		private:
+			friend struct ThreadData< Resource >;
+
 			// Private helper function to insert a new resource into the map and do error checking
 			Resource& InsertResource( const ResourceID id, const std::string& filename, std::unique_ptr< Resource > newResource );
 
-			std::map< const ResourceID, std::unique_ptr< Resource > > m_resourceMap;
+			ResourceMap m_resourceMap;
+			sf::Mutex m_resourceMutex;
+			std::vector< std::pair< sf::Thread, std::future< void > > > m_loadingThreads;
 		};
 
 		// Template functions
@@ -49,6 +65,32 @@ namespace Reflex
 		}
 
 		template< typename Resource >
+		void LoadResourceAsync( void* ptr )
+		{
+			const auto data = *static_cast< typename ResouceManager< Resource >::ThreadData* >( ptr );
+
+			auto newResource = std::make_unique< Resource >();
+
+			if( !newResource->loadFromFile( data.filename ) )
+				THROW( "Failed to load " << data.filename );
+
+			sf::Lock lock( data.manager->m_resourceMutex );
+			data.manager->m_resourceMap->insert( std::make_pair( data.id, std::move( newResource ) ) );
+		}
+
+		template< typename Resource >
+		void ResouceManager< Resource >::LoadResourceAsync( const ResourceID id, const std::string& filename )
+		{
+			ThreadData data = { this, id, filename };
+			std::packaged_task<void()> task( []() { LoadResourceAsync< Resource >( &data ); } );
+			sf::Thread thread( &task );
+
+			//m_loadingThreads.emplace_back( &LoadResourceAsync< Resource >, ( void* )&data );
+			m_loadingThreads.emplace_back( std::pair( thread, task.get_future() ) );
+			m_loadingThreads.back().launch();
+		}
+
+		template< typename Resource >
 		template< typename Parameter >
 		Resource& ResouceManager< Resource >::LoadResource( const ResourceID id, const std::string& filename, const Parameter& secondParam )
 		{
@@ -58,6 +100,13 @@ namespace Reflex
 				THROW( "Failed to load " << filename );
 
 			return InsertResource( id, filename, std::move( newResource ) );
+		}
+
+		template< typename Resource >
+		template< typename Parameter >
+		void ResouceManager< Resource >::LoadResourceAsync( const ResourceID id, const std::string& filename, const Parameter& secondParam )
+		{
+
 		}
 
 		template< typename Resource >
